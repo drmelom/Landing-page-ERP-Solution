9b82d8a3e8c9fb023c3edac6c6e8cfeef73e1f3b
drmelom <drmelom@correo.udistrital.edu.co>
2023-01-12 19:50:00 -0500

    Queda completa toda la parte Front-end de la pagina

--- Landing-page/javascript/transitions.js
+++ Landing-page/javascript/transitions.js
@@ -3,3 +3,17 @@ window.addEventListener("scroll",function(){
     header.classList.toggle("box",window.scrollY>0);
 
 })
+addEventListener("DOMContentLoaded", function(){
+    const btn_menu = document.querySelector(".btn-menu")
+    if(btn_menu){
+
+        btn_menu.addEventListener("click", function(){
+            const main_menu = document.querySelector('.main-menu')
+            main_menu.classList.toggle("show")
+    
+        })
+    }
+    
+
+
+})
